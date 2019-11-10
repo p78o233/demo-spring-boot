2eefe97791031368f89d32bcee5eee62f9fde74e
吴翰文 <p78o233@126.com>
2019-11-10 14:37:51 +0800

    sql更改

--- src/main/resources/sql/test.sql
+++ src/main/resources/sql/test.sql
@@ -10,10 +10,43 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2019-10-18 08:56:34
+Date: 2019-11-10 14:37:04
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `pt_user`
+-- ----------------------------
+DROP TABLE IF EXISTS `pt_user`;
+CREATE TABLE `pt_user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '兼职用户表',
+  `userName` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '真实姓名',
+  `tel` varchar(15) DEFAULT NULL,
+  `wxCode` varchar(20) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `qqCode` varchar(20) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `nickName` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '昵称',
+  `pwd` varchar(36) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `token` varchar(36) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `province` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `city` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `area` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `address` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `score` int(11) DEFAULT '100' COMMENT '信用积分',
+  `emergencyContact` int(11) DEFAULT NULL COMMENT '紧急联系人关系 1 父亲 2母亲 3配偶 4朋友',
+  `emergencyTel` varchar(15) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '紧急联系人电话',
+  `idNum` varchar(18) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '身份证号码',
+  `gender` int(11) DEFAULT NULL COMMENT '性别 1男 2 女 3其他',
+  `idCardFront` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '身份证国徽面',
+  `idCardBack` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '身份证人像面',
+  `withIdCard` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '人和身份证一起',
+  `createTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of pt_user
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for `test`
